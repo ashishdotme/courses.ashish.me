ec58ddda583daea746ee77666c953133fdc91793
Ashish Patel <ashishsushilpatel@gmail.com>
2019-11-16 19:11:03 +0530

    release: v1

---
+++ src/core/api/itemsList/itemsList.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+
+const ItemsListApi = () =>
+  axios
+    .get(
+      `https://api.ashish.me/courses`
+    )
+    .then(result => {
+      return result.data;
+    });
+
+export default ItemsListApi;
